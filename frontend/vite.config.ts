9ece9375fa28a18858f2e7b1fc4be286bc0341dc
instt0x13 <instt0x13@gmail.com>
2026-08-25 00:06:32 +0300

    chore: 🔧обновил другие фалйы настроек фронтенда

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), tsconfigPaths()],
   server: {
     watch: {
       usePolling: true, // В Docker это важно!
