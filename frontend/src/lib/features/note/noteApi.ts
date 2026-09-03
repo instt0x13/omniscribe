5bf5602d27467a1ebba5757d1b47adb089f458fd
instt0x13 <instt0x13@gmail.com>
2026-09-03 12:22:10 +0300

    refactor: ♻️ URL API перенесен в appConfig для более удобного управления конфигурацией

--- frontend/src/lib/features/note/noteApi.ts
+++ frontend/src/lib/features/note/noteApi.ts
@@ -1,15 +1,15 @@
-const API_URL = "http://localhost:8000/notes";
+import { appConfig } from '$lib/shared/config';
 
 import type { Note } from './noteTypes';
 
 export async function fetchNotes(): Promise<Note[]> {
-  const res = await fetch(API_URL);
+  const res = await fetch(appConfig.apiBaseUrl);
   if (!res.ok) throw new Error("Ошибка загрузки");
   return res.json();
 }
 
 export async function createNote(note: Omit<Note, "id">): Promise<void> {
-  await fetch(API_URL, {
+  await fetch(appConfig.apiBaseUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(note),
@@ -17,7 +17,7 @@ export async function createNote(note: Omit<Note, "id">): Promise<void> {
 }
 
 export async function updateNote(id: number, note: Omit<Note, "id">): Promise<void> {
-  await fetch(`${API_URL}/${id}`, {
+  await fetch(`${appConfig.apiBaseUrl}/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(note),
---
+++ frontend/src/lib/shared/config/appConfig.ts
@@ -0,0 +1,4 @@
+export const appConfig = {
+  apiBaseUrl: "http://localhost:8000/notes",
+  appName: 'OmniScribe'
+} as const;
