6dcc63c53e2be50bbbd13608b8bc1c6c5b45b9da
instt0x13 <instt0x13@gmail.com>
2026-09-17 20:09:40 +0300

    fix: 🐛 SplitColumn добавлен в index

--- frontend/src/lib/shared/ui/layout/index.ts
+++ frontend/src/lib/shared/ui/layout/index.ts
@@ -1,2 +1,3 @@
-export { default as Header } from "./Header.svelte";
+export { default as Header } from "./Header.svelte"
 export { default as SplitRow } from "./SplitRow.svelte"
+export { default as SplitColumn } from "./SplitColumn.svelte"
