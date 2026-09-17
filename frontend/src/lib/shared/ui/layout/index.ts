d382a20484e0a27d34b55eb0372b56512789c2b2
instt0x13 <instt0x13@gmail.com>
2026-09-17 20:52:56 +0300

    style: 💄 добавлены недостающие точки с запятой в index.ts

--- frontend/src/lib/shared/ui/layout/index.ts
+++ frontend/src/lib/shared/ui/layout/index.ts
@@ -1,3 +1,3 @@
-export { default as Header } from "./Header.svelte"
-export { default as SplitRow } from "./SplitRow.svelte"
-export { default as SplitColumn } from "./SplitColumn.svelte"
+export { default as Header } from "./Header.svelte";
+export { default as SplitRow } from "./SplitRow.svelte";
+export { default as SplitColumn } from "./SplitColumn.svelte";
--- frontend/src/lib/shared/ui/primitives/index.ts
+++ frontend/src/lib/shared/ui/primitives/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from "./Button.svelte";
-export { default as Icon } from "./Icon.svelte"
-export { default as GhostButton } from "./GhostButton.svelte"
+export { default as Icon } from "./Icon.svelte";
+export { default as GhostButton } from "./GhostButton.svelte";
